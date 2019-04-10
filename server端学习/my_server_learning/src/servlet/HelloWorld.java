1695eb7e1b7d5cb31eb8829ba093c33fd53f0f24
mojota <mojotatoo@gmail.com>
2019-04-10 15:26:26 +0800

    增加servlet练习

---
+++ server端学习/my_server_learning/src/servlet/HelloWorld.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * @author jamie
+ * @date 19-4-10
+ */
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("Servlet正在初始化,config: " + config.getServletName());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //专门向客服端提供响应的方法
+        System.out.println("Servlet正在提供服务");
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "你好，页面" + "</h1>");
+        Enumeration<String> hns = req.getHeaderNames();
+        while (hns.hasMoreElements()) {
+            String hn = hns.nextElement();
+            out.println(hn + ":" + req.getHeader(hn));
+            out.println("");
+        }
+        out.println(req.getParameter("p1"));
+        out.println("");
+        HttpSession session = req.getSession();
+        out.println(session.getAttribute("s1"));
+        out.println("");
+        session.setAttribute("s1","我是session1");
+
+        String p2 = req.getParameter("p2");
+        if (p2 != null) {
+//            resp.sendRedirect("index.jsp");
+//            resp.sendRedirect("http://www.baidu.com");
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("servlet销毁了");
+    }
+}
---
+++ server端学习/my_server_learning/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jamie
+  Date: 19-4-10
+  Time: 上午9:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  hello server
+  </body>
+</html>
