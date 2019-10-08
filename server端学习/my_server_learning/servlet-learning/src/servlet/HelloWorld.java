0411152b88a50ee999318095785c7dcbee135819
mojota <mojotatoo@gmail.com>
2019-10-08 15:56:12 +0800

    补充内容

--- server端学习/my_server_learning/servlet-learning/src/servlet/HelloWorld.java
+++ server端学习/my_server_learning/servlet-learning/src/servlet/HelloWorld.java
@@ -52,6 +52,16 @@ public class HelloWorld extends HttpServlet {
     }
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
     public void destroy() {
         super.destroy();
         System.out.println("servlet销毁了");
